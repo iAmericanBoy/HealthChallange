bb070cfae8ffb72ed9bb7d48348a9a32edc63778
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-11 12:34:29 -0600

    Adds update: and delete:

--- HealthChallenge/Controller/ModelController/UserController.swift
+++ HealthChallenge/Controller/ModelController/UserController.swift
@@ -43,10 +43,10 @@ class UserController {
         }
     }
     
-    ///Fetches the Logged in User
-    /// - parameter completion: Handler for when
-    /// - parameter isSuccess: Confirms that the change has synced to CloudKit.
-    func fetchUser(completion: @escaping (_ isSuccess:Bool) -> Void) {
+    ///Fetches the Logged in User in the publicDB.
+    /// - parameter completion: Handler for when the logged in user could be found.
+    /// - parameter isSuccess: Confirms that the Logged in User was found in the PublicDB.
+    func fetchUserLoggedInUser(completion: @escaping (_ isSuccess:Bool) -> Void) {
         
         CloudKitController.shared.fetchUserRecordID { (isSuccess, appleUserRef) in
             if isSuccess {
@@ -58,15 +58,45 @@ class UserController {
                 let query = CKQuery(recordType: User.typeKey, predicate: predicate)
                 CloudKitController.shared.findRecords(withQuery: query, inDataBase: CloudKitController.shared.publicDB, { (isSuccess, records) in
                     if isSuccess {
-                        //set logged in User to found record
+                        //set logged in User to found record.
                         guard let record = records.first else {completion(false); return }
                         self.loggedInUser = User(record: record)
                         completion(true)
                     } else {
-                        //no public user found
+                        //no public user found in the database.
                         completion(false)
                     }
                 })
+            }
+        }
+    }
+    
+    ///Updates the name or the Photo of a User and saves the change to CK.
+    /// - parameter user: The user to be updated.
+    /// - parameter name: Updated UserName of the user.
+    /// - parameter photo: Updated UserPhoto of the user.
+    /// - parameter completion: Handler for when the user was updated.
+    /// - parameter isSuccess: Confirms that the update has synced to CloudKit.
+    func update(user:User, withNewName name: String, andWithNewPhoto photo: UIImage , _ completion: @escaping (_ isSuccess:Bool) -> Void) {
+        user.photo = photo
+        user.userName = name
+        let updatedRecord = CKRecord(user: user)
+        
+        CloudKitController.shared.saveChangestoCK(inDataBase: CloudKitController.shared.publicDB, recordsToUpdate: [updatedRecord], purchasesToDelete: []) { (isSuccess, updatedRecords, _) in
+            if isSuccess {
+                guard let record = updatedRecords?.first, updatedRecord.recordID == record.recordID else {completion(false);return}
+                completion(true)
+            }
+        }
+    }
+    
+    ///Deletes the user from CK.
+    func delete(User user: User , _ completion: @escaping (_ isSuccess:Bool) -> Void) {
+        let deletedRecord = CKRecord(user: user)
+        CloudKitController.shared.saveChangestoCK(inDataBase: CloudKitController.shared.publicDB, recordsToUpdate: [], purchasesToDelete: [deletedRecord.recordID]) { (isSuccess, _, purchasesToDelete) in
+            if isSuccess {
+                guard let recordID = purchasesToDelete?.first, deletedRecord.recordID == recordID else {completion(false);return}
+                completion(true)
             }
         }
     }
