311805bc1ca78f9c9b097e8681bcb270516f2957
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-03-11 16:36:06 -0600

    fixes stuff hopefully

---
+++ HealthChallenge/Model/MagicStrings/MagicSrings.swift
@@ -0,0 +1,21 @@
+//
+//  MagicSrings.swift
+//  HealthChallenge
+//
+//  Created by RYAN GREENBURG on 3/11/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import Foundation
+extension User {
+    /// "User"
+    static let typeKey = "User"
+    /// "name"
+    static let usernameKey = "name"
+    /// "appleUserRef"
+    static let appleUserRefKey = "appleUserRef"
+    /// "photo"
+    static let photoKey = "photo"
+    /// "strengthValue"
+    static let strengthValueKey = "strengthValue"
+}
