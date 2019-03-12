e70b55fe4a87b793ef9bd2ccbb035e461b226d58
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-12 16:09:44 -0600

    Adds finishday as a computed property

--- HealthChallenge/Model/Challenge.swift
+++ HealthChallenge/Model/Challenge.swift
@@ -11,36 +11,43 @@ import CloudKit
 
 class Challenge {
     var startDay: Date
-    var finishDay: Date
     var weekGoalsReference: [CKRecord.Reference] = []
-    var name: String
     var recordID: CKRecord.ID
     
-    init(startDay: Date, recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
-        let startMonth = Calendar.current.monthSymbols[Calendar.current.component(.month, from: startDay)]
-        self.finishDay = Calendar.current.date(byAdding: .day, value: 30, to: startDay)!
-        let finishMonth = Calendar.current.monthSymbols[Calendar.current.component(.month, from: finishDay)]
-
-        if startMonth != finishMonth {
-            self.name = "\(startMonth)/\(finishMonth) Challange"
-        } else {
-            self.name = "\(startMonth) Challange"
+    var name: String
+    
+    var finishDay: Date {
+        get {
+            return Calendar.current.date(byAdding: .day, value: 30, to: startDay)!
         }
+    }
+
+    init(startDay: Date, recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
         self.startDay = startDay
         self.recordID = recordID
+        self.name = ""
+
+        let newName: String
+        let startMonth = Calendar.current.monthSymbols[Calendar.current.component(.month, from: startDay)]
+        let finishMonth = Calendar.current.monthSymbols[Calendar.current.component(.month, from: finishDay)]
+        
+        if startMonth != finishMonth {
+            newName = "\(startMonth)/\(finishMonth) Challange"
+        } else {
+            newName = "\(startMonth) Challange"
+        }
+        self.name = newName
     }
     
     init?(record: CKRecord) {
         guard let name = record[Challenge.nameKey] as? String,
             let startDay = record[Challenge.startDayKey] as? Date,
-            let finishDay = record[Challenge.finishDayKey] as? Date,
             let weekGoals = record[Challenge.weekGoalsKey] as? [CKRecord.Reference] else {return nil}
         
         self.name = name
         self.weekGoalsReference = weekGoals
         self.startDay = startDay
         self.recordID = record.recordID
-        self.finishDay = finishDay
     }
 }
 
@@ -51,7 +58,6 @@ extension CKRecord {
         
         self.setValue(challenge.name, forKey: Challenge.nameKey)
         self.setValue(challenge.startDay, forKey: Challenge.startDayKey)
-        self.setValue(challenge.finishDay, forKey: Challenge.finishDayKey)
         self.setValue(challenge.weekGoalsReference, forKey: Challenge.weekGoalsKey)
     }
 }
