1d070dda700ccf0a592067bbb11aa65987e8a93c
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-18 17:09:34 -0600

    Fixes ControlFlow of App

--- HealthChallenge/Controllers/ViewControllers/SignUp/HealthKitAccessViewController.swift
+++ HealthChallenge/Controllers/ViewControllers/SignUp/HealthKitAccessViewController.swift
@@ -31,18 +31,11 @@ class HealthKitAccessViewController: UIViewController {
     @IBAction func allowButtonTapped(_ sender: Any) {
         HealthKitController.shared.authorizeHK { (success) in
             //handle
-            self.showNewChallengeVC()
+            //pop to new user
         }
     }
     
     @IBAction func dontAllowButtonTapped(_ sender: Any) {
     }
     
-    func showNewChallengeVC() {
-        DispatchQueue.main.async {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let viewController = storyboard.instantiateViewController(withIdentifier: "NewChallengeParentViewController")
-            self.present(viewController, animated: true, completion: nil)
-        }
-    }
 } // end class
