5437e54127c0f52afd311d53d67e462cff39833d
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-03-21 16:48:32 -0600

    adds workout detailTableView, add workouts functionality.

---
+++ HealthChallenge/View/TabBarViews/WorkoutDetailTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  WorkoutDetailTableViewCell.swift
+//  HealthChallenge
+//
+//  Created by RYAN GREENBURG on 3/21/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutDetailTableViewCell: UITableViewCell {
+    
+    var workout: Workout? {
+        didSet {
+            updateViews()
+        }
+    }
+    weak var delegate: WorkoutDetailTableViewCellDelegate?
+
+    @IBOutlet weak var activityLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    func updateViews() {
+        guard let workout = workout else { return }
+        self.activityLabel.text = workout.activity
+        self.durationLabel.text = "\(workout.duration)"
+        self.dateLabel.text = "\(workout.end.day)/\(workout.end.day)"
+    }
+}
+
+protocol WorkoutDetailTableViewCellDelegate: class {
+}
