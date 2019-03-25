7e223b656efdc8c4d8b1987c8fe428eb71680789
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-24 21:30:33 -0600

    Adds sharing as a owner

--- HealthChallenge/Controllers/ViewControllers/Onboarding/NewOnboardingViewController.swift
+++ HealthChallenge/Controllers/ViewControllers/Onboarding/NewOnboardingViewController.swift
@@ -243,8 +243,7 @@ extension NewOnboardingViewController: UICollectionViewDataSource, UICollectionV
             //share
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "shareCell", for: indexPath) as? ShareCollectionViewCell
             
-            //            cell?.delegate = self
-            //            cell?.activeChallenge = ChallengeController.shared.currentChallenge
+            cell?.delegate = self
             
             return cell ?? UICollectionViewCell()
         default:
@@ -444,7 +443,37 @@ extension NewOnboardingViewController: ShareCollectionViewCellDelegate {
     func shareCurrentChallenge() {
         switch challengeState {
         case .isOwnerChallenge:
-            break
+            if let share = ChallengeController.shared.currentShare {
+                let sharingViewController = UICloudSharingController(share: share, container: CKContainer.default())
+                sharingViewController.delegate = self
+                
+                self.present(sharingViewController, animated: true)
+
+            } else {
+                guard let challenge = ChallengeController.shared.currentChallenge, let record = CKRecord(challenge: challenge) else {return}
+                let share = CKShare(rootRecord: record)
+                share.publicPermission = .readWrite
+                
+                let sharingViewController = UICloudSharingController(preparationHandler: {(UICloudSharingController, handler: @escaping (CKShare?, CKContainer?, Error?) -> Void) in
+                    let operation = CKModifyRecordsOperation(recordsToSave: [record,share], recordIDsToDelete: nil)
+                    operation.savePolicy = .changedKeys
+                    
+                    operation.modifyRecordsCompletionBlock = { (savedRecord, _,error) in
+                        handler(share, CKContainer.default(), error)
+                    }
+                    
+                    operation.perRecordCompletionBlock = { (savedRecord,error) in
+                        if let error = error {
+                            print(error)
+                        }
+                    }
+                    CloudKitController.shared.privateDB.add(operation)
+                })
+                
+                sharingViewController.delegate = self
+                
+                self.present(sharingViewController, animated: true)
+            }
         case .isParticipantChallenge:
             break
         case .noActiveChallenge:
@@ -457,6 +486,30 @@ extension NewOnboardingViewController: ShareCollectionViewCellDelegate {
         let viewController = storyboard.instantiateViewController(withIdentifier: "ActiveChallengeController")
         self.present(viewController, animated: true, completion: nil)
     }
+}
+
+//MARK: - UICloudSharingControllerDelegate
+extension NewOnboardingViewController: UICloudSharingControllerDelegate {
+    func cloudSharingControllerDidSaveShare(_ csc: UICloudSharingController) {
+        guard let url = csc.share?.url, let challenge = ChallengeController.shared.currentChallenge else {return}
+        ChallengeController.shared.add(stringURL: url.absoluteString, toChallenge: challenge) { (isSuccess) in
+            if isSuccess {
+                print("Succesfully added Url to Challenge")
+                print(url)
+            }
+        }
+        print("CKShare saved successfully")
+    }
     
+    func cloudSharingController(_ csc: UICloudSharingController, failedToSaveShareWithError error: Error) {
+        print("failed to save ckshare: \(error),\(error.localizedDescription)")
+    }
     
+    func itemThumbnailData(for csc: UICloudSharingController) -> Data? {
+        return nil //You can set a hero image in your share sheet. Nil uses the default.
+    }
+    
+    func itemTitle(for csc: UICloudSharingController) -> String? {
+        return ChallengeController.shared.currentChallenge?.name
+    }
 }
